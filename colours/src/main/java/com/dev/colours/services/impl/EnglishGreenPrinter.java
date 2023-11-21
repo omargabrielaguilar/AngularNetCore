5f00b3ef780033eb2d433823f28db9dc8ac92490
Omar <saucyfunnel@gmail.com>
2023-11-21 09:10:54 -0500

    demo

--- colours/src/main/java/com/dev/colours/services/impl/EnglishGreenPrinter.java
+++
@@ -1,9 +0,0 @@
-package com.dev.colours.services.impl;
-
-import com.dev.colours.services.GreenPinter;
-public class EnglishGreenPrinter implements GreenPinter {
-    @Override
-    public String print() {
-        return "green";
-    }
-}
